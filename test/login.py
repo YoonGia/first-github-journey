228339999011befff749674ad3a05f1ddec11722
经理 <manager@126.com>
2018-02-07 09:54:46 +0800

    v3.0开发结束

--- test/login.py
+++ test/login.py
@@ -6,3 +6,20 @@ num3 = 30
 
 jiejue = 'bug'
 
+<<<<<<< Updated upstream
+=======
+num5 = 50
+
+num6 = 6000
+
+num7 = 70
+
+num8 = 8000
+
+num = 1000
+
+num12 = 100
+
+
+vir = 10
+>>>>>>> Stashed changes
